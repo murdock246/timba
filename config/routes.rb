de97b31e07471ea91a393c23250142b94bdd9bfe
murdock246 <murdock246@gmail.com>
2014-09-24 19:20:04 -0500

    Page with info, team, clients says, and links to fb an twetts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   
   get 'welcome', to: 'pages#home'
-  get 'about', to: 'welcome#about_us'
-  get 'whatwedo', to: 'welcome#we_do'
-  get 'portafolio', to: 'welcome#porfolio'
-  get 'opensource', to: 'welcome#open_source'
   
-
 
   # You can have the root of your site routed with "root"
    root 'pages#home'
