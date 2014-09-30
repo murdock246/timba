26226cadba8491e7616768c3b1b5d42ea9c937c8
murdock246 <murdock246@gmail.com>
2014-09-30 10:50:49 -0500

    the username and the passwords of sendgrid are ready to be used

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,11 +1,12 @@
 class ContactUsMailer < ActionMailer::Base
   
   def contact_us_email(name, from, message)
+  	default to: 'david@timba.co'
   	
   	@name = name
   	@message = message
 
-  	mail(to: "david@timba.co", from: from, subject: 'Inquery from #{name}')
+  	mail(from: from, subject: 'Inquery from #{name}')
 
   end
 end
