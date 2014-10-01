4947d3b561b4332a4b17fce491e04648b5b9c2ec
murdock246 <murdock246@gmail.com>
2014-10-01 12:51:30 -0500

    the about_us page and the open_source page are working on the list with root

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,26 @@
 class PagesController < ApplicationController
-	def home
-  	end
+	
+  def home
+  end
 
-  	def contact
-  		
-  	end
+  def open_source
+  end
+  
 
-  	def blog
-  	end
+  def blog
+  end
 
-  	def portfolio
-  	end 	
+  def portfolio
+  end
 
-  	def send_contact_mail
-  		name = params[:name]
-  		mail = params[:email]
-  		message = params[:message]
-  		ContactUsMailer.contact_us_email(name, mail, message).deliver
-      redirect_to :home
-    end
+  def contact
+  end 	
+
+	def send_contact_mail
+		name = params[:name]
+		mail = params[:email]
+		message = params[:message]
+		ContactUsMailer.contact_us_email(name, mail, message).deliver
+    redirect_to :home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   
   get 'home', to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio'
+  get 'open_source', to: 'pages#open_source'
   get 'blog', to: 'pages#blog'
+  get 'about_us', to: 'pages#about_us'
   get 'contact', to: 'pages#contact'
   post 'contact', to: 'pages#send_contact_mail'
+  
 
 
   # You can have the root of your site routed with "root"
